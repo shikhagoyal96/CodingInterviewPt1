a037ab374975bf5c40d86eac9a8248aa1a260292
Shikha <Shikha Goyal>
2020-06-06 22:39:56 -0400

    task completed

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+    //link to the file to retrieve the data which is dtored in json format
+    $.getJSON("http://sandbox.bittsdevelopment.com/code1/fetchemployees.php", function(json)
+    {
+        //printing the json file in console section to check the fetching of the json data
+        console.log(json);
+        //running the loop 3 times as the file contains data for 3 members
+        //later we can make it scalable so that it can retrieve data from more than 'n' number of members
+        for(var i=1; i <=3; i++){
+            
+            //accessing image
+            var x = document.createElement("IMG");
+            //link to access the picture for each team member
+            x.setAttribute("src", "http://sandbox.bittsdevelopment.com/code1/employeepics/"+i+".jpg");
+            //setting the dimensions for the picture
+            x.setAttribute("width", "304");
+            x.setAttribute("height", "228");
+            x.setAttribute("alt", "Image");
+            document.getElementById("picture" +i).append(x);
+
+            //accessing the firstname of the ith team member
+            var fname = json[i].employeefname;
+            //accessing the lastname of the ith team member
+            var lname = json[i].employeelname;
+            //combining the first and last name to print the complete name of the team member
+            document.getElementById("name"+i).innerHTML = fname + " " + lname;
+            //accessing the bio of the employee
+            var bio = json[i].employeebio;
+            document.getElementById("bio"+i).innerHTML = bio;
+
+            //accessing the roles
+            for(var j=0; j<json[i].roles.length;j++){
+                var role = json[i].roles[j].rolename;
+
+                //creating a button to wrap the role content inside it to an appearance
+                var btn = document.createElement("BUTTON");   
+                btn.innerHTML +=role;               
+                document.getElementById("but"+i).append(btn);
+            }
+
+            //accessing the crown image for the employee which is featured
+            var featured = json[i].employeeisfeatured;
+            if(featured === "1"){
+                //accessing image
+                var y = document.createElement("img");
+                //setting the src for the crown image
+                y.setAttribute("src", "crown.jpg");
+                //setting the dimensions for the crown
+                y.setAttribute("width", "50");
+                y.setAttribute("height", "50");
+                y.setAttribute("alt", "crown");
+                document.getElementById("crown"+i).append(y);  
+            }
+        }  
+    });
+});
+
+
+
+
+
